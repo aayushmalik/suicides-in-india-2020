155ce8b630481d610bf9997beb2a77cfe7a47ad9
Aayush Malik <55054776+aayushmalik@users.noreply.github.com>
2021-11-10 14:10:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,162 @@
+import streamlit as st
+import pandas as pd
+import geopandas as gpd
+import plotly.express as px
+
+df = pd.read_excel("cause-of-suicide-Table 2.5 state-ut-city.xlsx", sheet_name = "Sheet1")
+
+df['Total'] = df['Male'] + df['Female']
+states = sorted(set(df['State']))
+causes = sorted(set(df['Cause']))
+
+india = gpd.read_file("./India_State_Boundary.shp")
+
+india.replace({
+    "Andaman & Nicobar": "A&N Islands",
+    "Jammu and Kashmir": "Jammu & Kashmir",
+    "Telengana": "Telangana",
+    "Tamilnadu": "Tamil Nadu",
+    "Chhattishgarh": "Chhattisgarh",
+    "Daman and Diu and Dadra and Nagar Haveli": "Dadra and Nagar Haveli and Daman and Diu"
+}, inplace = True)
+
+india.rename(columns = {
+    "Name": "State"
+}, inplace = True)
+
+india['State'] = india['State'].str.capitalize()
+df['State'] = df['State'].str.capitalize()
+
+geo_df = india.merge(df, on = "State").set_index("State")
+
+with st.sidebar:
+    st.title("India Suicides Data 2020")
+
+    choice = st.radio("Select one", ["All India", "State-wise Causes", "Causes Choropleth Map"])
+
+    st.write("Source: India Suicide Data 2020, National Crime Records Bureau, Government of India")
+
+if choice == "All India":
+    st.header("All India Level")
+
+    # all_india_fig = px.choropleth(
+    #     geo_df,
+    #     geojson = geo_df.geometry,
+    #     locations = geo_df.index,
+    #     color = "Total",
+    #     projection = 'mercator',
+    #     height = 600,
+    #     width = 600
+    # )
+
+    # all_india_fig.update_geos(fitbounds = "locations", visible=True)
+
+    # st.subheader("Chart showing the total number of suicides")
+    # st.plotly_chart(all_india_fig, use_container_width=True)
+
+    top_10_states = df.groupby('State')['Total'].sum().nlargest(10).sort_values(ascending = True)
+    top_10_states_fig = px.bar(
+        top_10_states,
+        orientation = 'h',
+        title = 'Top 10 States of Suicides in India in 2020',
+        labels = {
+            "value": "Total Cases",
+            "variable": "Total Cases"
+        }
+    )
+    st.plotly_chart(top_10_states_fig)
+
+    top_10_causes = df.groupby('Cause')['Total'].sum().nlargest(10).sort_values(ascending = True)
+    top_10_causes_fig = px.bar(
+        top_10_causes,
+        orientation = 'h',
+        title = 'Top Causes of Suicides in India in 2020',
+        labels = {
+            "value": "Total Cases",
+            "variable": "Total Cases"
+        }
+    )
+    st.plotly_chart(top_10_causes_fig)
+
+    top_10_causes_female = df.groupby('Cause')['Female'].sum().nlargest(10).sort_values(ascending = True)
+    top_10_causes_female_fig = px.bar(
+        top_10_causes_female,
+        orientation = 'h',
+        title = 'Top Causes of Suicides for Females in India in 2020',
+        labels = {
+            "value": "Total Cases",
+            "variable": "Total Cases"
+        }
+    )
+    st.plotly_chart(top_10_causes_female_fig)
+
+    top_10_causes_male = df.groupby('Cause')['Male'].sum().nlargest(10).sort_values(ascending = True)
+    top_10_causes_male_fig = px.bar(
+        top_10_causes_male,
+        orientation = 'h',
+        title = 'Top Causes of Suicides for Males in India in 2020',
+        labels = {
+            "value": "Total Cases",
+            "variable": "Total Cases"
+        }
+    )
+    st.plotly_chart(top_10_causes_male_fig)
+
+elif choice == "State-wise Causes":
+    state = st.sidebar.selectbox('Select State', states)
+    df = df[df['State'] == state].sort_values('Total', ascending = False)
+
+    top_10_causes = df.groupby('Cause')['Total'].sum().nlargest(10).sort_values(ascending = True)
+    top_10_causes_fig = px.bar(
+        top_10_causes,
+        orientation = 'h',
+        title = f'Top Causes of Suicides in {state} in 2020',
+        labels = {
+            "value": "Total Cases",
+            "variable": "Total Cases"
+        }
+    )
+    st.plotly_chart(top_10_causes_fig)
+
+    top_10_causes_female = df.groupby('Cause')['Female'].sum().nlargest(10).sort_values(ascending = True)
+    top_10_causes_female_fig = px.bar(
+        top_10_causes_female,
+        orientation = 'h',
+        title = f'Top Causes of Suicides for Females in {state} in 2020',
+        labels = {
+            "value": "Total Cases",
+            "variable": "Total Cases"
+        }
+    )
+    st.plotly_chart(top_10_causes_female_fig)
+
+    top_10_causes_male = df.groupby('Cause')['Male'].sum().nlargest(10).sort_values(ascending = True)
+    top_10_causes_male_fig = px.bar(
+        top_10_causes_male,
+        orientation = 'h',
+        title = f'Top Causes of Suicides for Males in {state} in 2020',
+        labels = {
+            "value": "Total Cases",
+            "variable": "Total Cases"
+        }
+    )
+    st.plotly_chart(top_10_causes_male_fig)
+
+else:
+    cause = st.sidebar.selectbox('Select Cause', causes)
+
+    cdf = geo_df[geo_df['Cause'] == cause]
+
+    cause_fig = px.choropleth(
+        cdf,
+        geojson = cdf.geometry,
+        locations = cdf.index,
+        color = "Total",
+        projection = 'mercator',
+        height = 600,
+        width = 600
+    )
+
+    cause_fig.update_geos(fitbounds = "locations", visible=True)
+    st.subheader(f"Chart showing the number of suicides due to {cause}")
+    st.plotly_chart(cause_fig, use_container_width=True)
